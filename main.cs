a98cc2ff5b036954e67e77fc6cd44529c4c9cdb2
JerickaDanielle <158662351+JerickaDanielle@users.noreply.github.com>
2024-02-24 03:51:38 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        string message = "";
+
+        while (true)
+        {
+            Console.WriteLine("Enter something:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+            {
+                Console.WriteLine("Closing Program...");
+                break;
+            }
+
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                sum += number;
+                Console.WriteLine($"Adding {number} to {sum - number} = {sum}");
+            }
+            else
+            {
+                message += input;
+                Console.WriteLine($"Current Message: {message}");
+            }
+        }
+    }
+}
